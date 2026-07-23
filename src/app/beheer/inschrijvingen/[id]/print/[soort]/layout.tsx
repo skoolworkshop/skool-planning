555c1b84d199aff92c162226e3e64874ef5823b5
skoolworkshop <info@skoolworkshop.nl>
2026-07-23 15:28:47 +0200

    Delete src directory

--- src/app/beheer/inschrijvingen/[id]/print/[soort]/layout.tsx
+++
@@ -1,5 +0,0 @@
-import "@/app/globals.css";
-
-export default function PrintLayout({ children }: { children: React.ReactNode }) {
-  return <div className="min-h-screen bg-white">{children}</div>;
-}
