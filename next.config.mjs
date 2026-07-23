698fef85aae4ad2122c414815529bc577882fdcd
skoolworkshop <info@skoolworkshop.nl>
2026-07-23 15:30:16 +0200

    Delete next.config.mjs

--- next.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    remotePatterns: [{ protocol: "https", hostname: "skoolworkshop.nl" }, { protocol: "https", hostname: "www.skoolworkshop.nl" }],
-  },
-};
-export default nextConfig;
